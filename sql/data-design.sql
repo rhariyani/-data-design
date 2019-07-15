58c575759662c4658d8d5fdbdb5a4fcb8fff9857
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-15 16:10:44 -0600

    Phase3 Initial submission

---
+++ sql/data-design.sql
@@ -0,0 +1,28 @@
+ALTER DATABASE rhariyani CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS connection;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile(
+	profileId BINARY(16) NOT NULL,
+	UserName VARCHAR(100) not null,
+	UserPassward char(97) NOT NULL,
+	Name VARCHAR(50) NOT NULL,
+	Gender VARCHAR(50),
+	Age TINYINT,
+	Education VARCHAR(128),
+	Awards  VARCHAR(128),
+	Occupation VARCHAR(128),
+	Likes VARCHAR(128),
+	Dislikes VARCHAR(128),
+	URL VARCHAR(128),
+	PRIMARY KEY(profileId)
+);
+
+CREATE TABLE connection(
+	ConnectionType VARCHAR(120),
+	DateofConnection DATETIME,
+	profileId BINARY(16) NOT NULL,
+		FOREIGN KEY (profileId) References profile(profileId)
+	);
+
